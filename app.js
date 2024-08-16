bad73a3e7ba6a39e4d47dede19c5752f6e8c92cd
MandarAcharekar <mandarmva8055@gmail.com>
2024-08-16 23:33:40 +0530

    basic login & register done

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
